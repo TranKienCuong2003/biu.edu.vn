3bd993e181389e4a809b85315fb1e27f186dd5c4
Trần Kiên Cường <trankiencuong30072003@gmail.com>
2024-06-13 20:57:25 +0700

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+
+window.location.href = 'http://127.0.0.1:5500/#trangchu';
+
+const sliderContainer = document.querySelector('.container__slider');
+const slides = document.querySelectorAll('.container__slider-product');
+
+let slideIndex = 0;
+const slideInterval = 5000;
+
+function nextSlide() {
+    slideIndex = (slideIndex + 1) % slides.length;
+    updateSlide();
+}
+
+function updateSlide() {
+    sliderContainer.style.transition = 'transform 0.5s ease-in-out';
+    sliderContainer.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
+
+function preloadImages() {
+    slides.forEach((slide, index) => {
+        const img = slide.querySelector('.container__slider-img');
+        if (!img.complete) {
+            img.onload = () => {
+                if (index === slideIndex) {
+                    updateSlide();
+                }
+            };
+        }
+    });
+}
+
+// Preload images and start the slider
+preloadImages();
+setInterval(nextSlide, slideInterval);
+updateSlide();
+
+window.addEventListener('load', () => {
+    const letters = document.querySelectorAll('.container__student-life-other-informations-specifically-letter');
+
+    letters.forEach(letter => {
+        const img = letter.previousElementSibling;
+        const imgHeight = img.clientHeight;
+
+        letter.style.height = `${imgHeight}px`;
+    });
+});
